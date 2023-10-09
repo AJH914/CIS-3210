676f36592bce0c9998d588de3b90123f354a48fb
Robert Stegmann <rstegman@uoguelph.ca>
2023-10-09 16:26:27 -0400

    Almost finished send file and progress server
    Co-authored-by: Andrew Heft <AJH914@users.noreply.github.com>

--- sendFile.c
+++ sendFile.c
@@ -11,12 +11,19 @@ int main(int argc, char *argv[])
     char * tempArg;
     char * address;
     char * port;
+    char * received;
+    int rLength;
+    char * readBuffer;
 
     if (argc == 3 || argc == 4) {
         fileName = argv[1];
+        char* suffix = strrchr(fileName, '.');
+        if (strcasecmp(suffix + 1, "txt") != 0) {
+            printf("The file is not a .txt file.\n");
+            exit(-1);
+        }
 
-        dest.sin_family = AF_INET;	
-        
+        dest.sin_family = AF_INET; // Use the IPv4 address family
         
         strcpy(tempArg, argv[2]);
         char * token = strtok(tempArg,":");
@@ -33,41 +40,89 @@ int main(int argc, char *argv[])
         }
 
         strcpy(port,token);
-
+        /*
+        Figures this out later for 2%
         error = getaddrinfo(address,port,&hints,&res);
         if (error != 0) {
             fprintf(stderr, "%s: %s\n", address, gai_strerror(error));
             exit(-1);
         }
         
-
-
-        inet_aton(token, &dest.sin_addr);
-        dest.sin_port = htons(atoi(token));  
-        //Initialize the destination socket information
-        				   // Use the IPv4 address family
-        if (argc == 1){
-            dest.sin_addr.s_addr = htonl(INADDR_LOOPBACK); // Set destination IP number - localhost, 127.0.0.1
-        }else if (argc == 2){
-            inet_aton(argv[1], &dest.sin_addr); // Set destination IP number from command line arg
-        }else{
-            printf("Invalid number of arguments\n");
-            printf("Usage: %s <ip>, where ip is an optional IP4 address in the dot-decimal notation\n", argv[0]);
-            printf("If the IP address is not proided, client attempts to connect to localhost\n");
-            exit(0);
+        if (res->ai_addr->sa_family == AF_INET) {
+            struct sockaddr_in *p = (struct sockaddr_in *)res->ai_addr;
+            printf("%s\n", inet_ntop(AF_INET, &p->sin_addr, str, sizeof(str)));
+        } else if (i->ai_addr->sa_family == AF_INET6) {
+            struct sockaddr_in6 *p = (struct sockaddr_in6 *)res->ai_addr;
+            printf("%s\n", inet_ntop(AF_INET6, &p->sin6_addr, str, sizeof(str)));
         }
-
-	    //dest.sin_port = htons(PORTNUM);                // Set destination port number
-
-
-
+        */
+        
+        inet_aton(address, &dest.sin_addr);
+        dest.sin_port = htons(atoi(port));
+        //Initialize the destination socket information
         if (argc == 4) {
             bufSize = atoi(argv[3]);
         }
     } else {
         printf("Invalid argument format. Input should be formatted as  $./sendFile fileName server-IP-address:port-number bufSize. \n");
-        exit(0);
+        exit(-1);
+    }
+
+    int mysocket = socket(dest.sin_family, SOCK_STREAM, 0);
+
+    // Connect to the server
+	error = connect(mysocket, (struct sockaddr *)&dest, sizeof(struct sockaddr_in));
+    if (error != 0) {
+        fprintf(stderr,"Error Connecting to Server: %s\n",strerror(errno));
+        close(mysocket);
+        exit(-1);
+    }
+
+    FILE * fptr;
+    fptr = fopen(fileName,"r");
+
+    if (fptr == NULL) {
+        fprintf(stderr,"Error: File %s failed to open: %s\n",fileName,strerror(errno));
+        close(mysocket);
+        exit(-1);
+    }
+
+    fseek(fptr, 0, SEEK_END);
+    int file_size = ftell(fptr);
+    fseek(fptr, 0, SEEK_SET);
+    char * initialMessage = malloc(bufSize*sizeof(char));
+    if (bufSize < snprintf(initialMessage, bufSize,"%s %d\n",fileName,file_size)) {
+        fprintf(stderr,"Error: File name %s and length too large for buffer length %d\n",fileName,bufSize);
+        free (initialMessage);
+        fclose(fptr);
+        close(mysocket);
+        exit(-1);
+    }
+
+    // Send initial message
+    send(mysocket, initialMessage, strlen(initialMessage), 0); 
+    free(initialMessage);
+
+    received = malloc(bufSize*sizeof(char)+1);
+    // Confirm message was recieved
+	rLength = recv(mysocket, received, bufSize, 0);
+    received[rLength] = '\0';
+
+    /* Put error check here*/
+
+    readBuffer = malloc(bufSize*sizeof(char));
+    while (fread(readBuffer, bufSize, sizeof(char), fptr) != NULL) {
+        send(mysocket, readBuffer, strlen(readBuffer), 0);  
     }
 
 
+    
+
+    rLength = recv(mysocket, received, bufSize, 0);
+    received[rLength] = '\0';
+
+    /* Put error check here*/
+    fclose(fptr);
+    free(readBuffer);
+    close(mysocket);
 }
--- server.c
+++ server.c
@@ -1,9 +1,51 @@
 #include "protocol.h"
 
+#define PORTNUM 50940
+
+int mysocket;            // socket used to listen for incoming connections
+int consocket;
+
+static void sigintCatcher(int signal,  siginfo_t* si, void *arg)
+{
+    printf("\n\n************** Caught SIG_INT: shutting down the server ********************\n");
+
+	close(consocket);
+	close(mysocket);
+	exit(0);
+}
+
 int main(int argc, char *argv[])
 {
     int bufSize = DEFAULT_BUFFER;
     char * port;
+
+    struct sockaddr_in dest; // socket info about the machine connecting to us
+	struct sockaddr_in serv; // socket info about our server
+    struct sigaction signaler;
+
+
+    socklen_t socksize = sizeof(struct sockaddr_in);
+
+	memset(&serv, 0, sizeof(serv));           // zero the struct before filling the fields
+	
+	serv.sin_family = AF_INET;                // Use the IPv4 address family
+	serv.sin_addr.s_addr = htonl(INADDR_ANY); // Set our address to any interface
+    serv.sin_port = htons(atoi(port));        // Set the server port number 
+    mysocket = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (bind(mysocket, (struct sockaddr *)&serv, sizeof(struct sockaddr)) != 0){
+		printf("Unable to open TCP socket on localhost:%d\n", PORTNUM);
+
+	/* 	strerror() returns a string representation of the system variable "errno"
+	 	errno is the integer code of the error that occured during the last system call from this process
+	 	need to include errno.h to use this function
+		*/
+		printf("%s\n", strerror(errno));
+		close(mysocket);
+		return 0;
+	}
+
+    listen(mysocket, 0);
 
     if (argc == 2 || argc == 3) {
         port = argv[2];
